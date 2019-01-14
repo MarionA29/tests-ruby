fa7166f6e2a4104e303e35525f638256b06805cc
Nasim Ibrahimi <46283064+N-Ibrahimi@users.noreply.github.com>
2019-01-14 23:16:26 +0100

    Update 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,7 +1,12 @@
-def s
-
+def translate(word)
+    lesmots = word.split(" ").map! do |i| 
+    if i.start_with?(/[b-df-hj-np-q(qu)r-tv-xz]++/)
+        i.insert(-1, i.match(/[b-df-hj-np-q(qu)r-tv-xz]++/).to_s)
+        i.slice!(i.match(/[b-df-hj-np-q(qu)r-tv-xz]++/).to_s)
+    end
+    i << 'ay'
+    end
+    return lesmots.join(" ")
 end
 
-def translate
-  
-end
+
